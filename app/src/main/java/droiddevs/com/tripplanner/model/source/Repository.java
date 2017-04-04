2b2549454db13eaae46801400a1cb17c798a63d5
Elmira Andreeva <elmira.andreeva@gmail.com>
2017-04-04 10:09:22 -0700

    add Trip parse object

--- app/src/main/java/droiddevs/com/tripplanner/model/source/Repository.java
+++ app/src/main/java/droiddevs/com/tripplanner/model/source/Repository.java
@@ -1,8 +1,19 @@
 package droiddevs.com.tripplanner.model.source;
 
+import droiddevs.com.tripplanner.model.source.local.LocalDataSource;
+import droiddevs.com.tripplanner.model.source.remote.RemoteDataSource;
+
 /**
  * Created by elmira on 4/3/17.
  */
 
 public class Repository implements DataSource {
+
+    private LocalDataSource localDataSource;
+    private RemoteDataSource remoteDataSource;
+
+    public Repository(LocalDataSource localDataSource, RemoteDataSource remoteDataSource) {
+        this.localDataSource = localDataSource;
+        this.remoteDataSource = remoteDataSource;
+    }
 }
