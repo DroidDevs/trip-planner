6670298c844fc89b0a620f7d390838787929636b
Elmira Andreeva <elmira.andreeva@gmail.com>
2017-04-04 10:15:04 -0700

    add TripApp instance to setup the Parse

---
+++ app/src/main/java/droiddevs/com/tripplanner/TripPlannerApp.java
@@ -0,0 +1,18 @@
+package droiddevs.com.tripplanner;
+
+import android.app.Application;
+
+/**
+ * Created by elmira on 4/4/17.
+ */
+
+public class TripPlannerApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //Parse.enableLocalDatastore(this);
+        //Parse.initialize(this);
+    }
+}
